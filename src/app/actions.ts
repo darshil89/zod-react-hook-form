29a1578d933c66f56af618bc37697a2df76bcfdc
darshil89 <darshilmahraur67@gmail.com>
2023-10-31 12:53:36 +0530

    removed try catch

--- src/app/actions.ts
+++ src/app/actions.ts
@@ -8,28 +8,22 @@ type Inputs = z.infer<typeof schema>;
 
 export async function addEntry(data: Inputs) {
 
-    try {
+    const result = await schema.safeParseAsync(data);
 
-        const result = await schema.safeParseAsync(data);
-
-        if (result.success) {
-            await connectToDB();
-            const user = await prisma.user.create({
-                data: {
-                    firstName: result.data.firstName,
-                    lastName: result.data.lastName,
-                    email: result.data.email,
-                    age: result.data.age,
-                },
-            });
-            return { success: true, data: user };
-        } else {
-            return { success: false, errors: result.error };
-        }
-    } catch (error: any) {
-        throw new Error(error)
+    if (result.success) {
+        await connectToDB();
+        const user = await prisma.user.create({
+            data: {
+                firstName: result.data.firstName,
+                lastName: result.data.lastName,
+                email: result.data.email,
+                age: result.data.age,
+            },
+        });
+        return { success: true, data: user };
+    } else {
+        return { success: false, errors: result.error };
     }
-
 
 
 

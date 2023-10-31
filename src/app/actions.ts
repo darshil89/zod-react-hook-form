1872d659616c665b2718f79a3c6a1e6726d68bc4
darshil89 <darshilmahraur67@gmail.com>
2023-10-31 13:34:10 +0530

    added missing fields

--- src/app/actions.ts
+++ src/app/actions.ts
@@ -12,17 +12,18 @@ export async function addEntry(data: Inputs) {
     if (result.success) {
         await connectToDB();
         const user = await prisma.user.create({
-            data: { 
+            data: {
                 firstName: result.data.firstName,
                 lastName: result.data.lastName,
                 email: result.data.email,
                 age: result.data.age,
-               
+                password : result.data.password,
+                confirmPassword:result.data.confirmPassword
             },
         })
         return { success: true, data: user }
     } else {
         console.log(result.error);
-        return { success: false, error: result.error}
+        return { success: false, error: result.error }
     }
 }
